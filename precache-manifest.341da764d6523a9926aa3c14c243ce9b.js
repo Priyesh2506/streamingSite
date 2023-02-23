1534964f46cc7bf2d48255d53ba8279da3f2db28
Priyesh Pandey <priyeshpandey559@gmail.com>
2023-02-23 16:03:19 +0530

    Updates

---
+++ precache-manifest.341da764d6523a9926aa3c14c243ce9b.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba395c9e7840501caa9d",
+    "url": "/streamingSite/static/css/main.4adeb958.chunk.css"
+  },
+  {
+    "revision": "ba395c9e7840501caa9d",
+    "url": "/streamingSite/static/js/main.ba395c9e.chunk.js"
+  },
+  {
+    "revision": "efef7f6fa880e78d2a4c",
+    "url": "/streamingSite/static/js/1.efef7f6f.chunk.js"
+  },
+  {
+    "revision": "68eca276d28fc2775554",
+    "url": "/streamingSite/static/css/2.aa0f24f2.chunk.css"
+  },
+  {
+    "revision": "68eca276d28fc2775554",
+    "url": "/streamingSite/static/js/2.68eca276.chunk.js"
+  },
+  {
+    "revision": "784d659e94920eb13af4",
+    "url": "/streamingSite/static/js/runtime~main.784d659e.js"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/streamingSite/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "ef725189737475f6646c1f1973eaa833",
+    "url": "/streamingSite/static/media/m15.ef725189.jpeg"
+  },
+  {
+    "revision": "49cf3b8de3910cd9819c4c73507ce9d4",
+    "url": "/streamingSite/static/media/m14.49cf3b8d.jpeg"
+  },
+  {
+    "revision": "0dcbd3c0b5d0f14ae432e55fbdd307f1",
+    "url": "/streamingSite/static/media/18.0dcbd3c0.png"
+  },
+  {
+    "revision": "678f5f8f290743c67b99329757f8ad00",
+    "url": "/streamingSite/static/media/m13.678f5f8f.jpeg"
+  },
+  {
+    "revision": "a1a82b3f667ad400fefded8008299f6e",
+    "url": "/streamingSite/static/media/m8.a1a82b3f.jpeg"
+  },
+  {
+    "revision": "18db0943cca77a07591bb430cee7bb66",
+    "url": "/streamingSite/static/media/m1.18db0943.jpeg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/streamingSite/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "c57547abc99c013d6bb97b6b896d86fb",
+    "url": "/streamingSite/static/media/17.c57547ab.png"
+  },
+  {
+    "revision": "368c2532a0d93ad305a2fbecfb532f01",
+    "url": "/streamingSite/static/media/m12.368c2532.jpeg"
+  },
+  {
+    "revision": "74b01b8be7fae0026688410c60e1d38f",
+    "url": "/streamingSite/static/media/m11.74b01b8b.jpeg"
+  },
+  {
+    "revision": "4135e6241371782f8d98822a87bd2fd5",
+    "url": "/streamingSite/static/media/m10.4135e624.jpeg"
+  },
+  {
+    "revision": "5e1b274c1032a23ec084f02587ca826f",
+    "url": "/streamingSite/static/media/m9.5e1b274c.jpeg"
+  },
+  {
+    "revision": "ae4e0568093a5359c975511858ae0ac5",
+    "url": "/streamingSite/static/media/m7.ae4e0568.jpeg"
+  },
+  {
+    "revision": "ddc3e255c257459be69d61c22178d64e",
+    "url": "/streamingSite/static/media/m6.ddc3e255.jpeg"
+  },
+  {
+    "revision": "33a4988dea3f582e84d1a76d73874b07",
+    "url": "/streamingSite/static/media/m5.33a4988d.jpeg"
+  },
+  {
+    "revision": "fb71e551cbf865911264e0e8b1d32175",
+    "url": "/streamingSite/static/media/m4.fb71e551.jpeg"
+  },
+  {
+    "revision": "6b34a33aa0d856db9b7b24157c971302",
+    "url": "/streamingSite/static/media/m3.6b34a33a.jpeg"
+  },
+  {
+    "revision": "bdeb9a93e4d82848df46b08fcc01ae11",
+    "url": "/streamingSite/static/media/m2.bdeb9a93.jpeg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/streamingSite/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/streamingSite/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/streamingSite/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/streamingSite/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/streamingSite/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "7b1e290bb35af94a71373e808d1a3564",
+    "url": "/streamingSite/static/media/18.7b1e290b.jpg"
+  },
+  {
+    "revision": "2c8da737aad41a3b3107a581e3569703",
+    "url": "/streamingSite/static/media/15.2c8da737.jpg"
+  },
+  {
+    "revision": "c4d123e2350fbf323b97c824f4edebac",
+    "url": "/streamingSite/index.html"
+  }
+];
